fe1e55ffaaedf3455571761a67f83664a011e389
Wasun Patrat <wasun.p@cmu.ac.th>
2024-11-18 09:40:00 +0700

    init: init exam repo

---
+++ app.vue
@@ -0,0 +1,19 @@
+<!-- <template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template> -->
+
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+  <ClientOnly>
+    <UNotifications />
+    <BackToTop />
+    <Loading />
+  </ClientOnly>
+</template>
+
+<script setup lang="ts"></script>
---
+++ pages/login/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: "blank",
+});
+
+async function login() {}
+</script>
