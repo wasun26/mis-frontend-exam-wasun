7fcbe6aa9325f07be5d16e75b655f2ab78b33864
Wasun Patrat <wasun.p@cmu.ac.th>
2024-11-18 10:02:17 +0700

    chore: add login page

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class=""></div>
+  <div class="flex items-center justify-center">
+    <div>
+      <UCard>
+        <template #header>
+          <span class="flex justify-center h1">เข้าสู่ระบบ</span>
+        </template>
+
+        <UForm
+          :schema="schema"
+          :state="state"
+          class="space-y-4"
+          @submit="onSubmit"
+        >
+          <UFormGroup label="ผู้ใช้" name="email">
+            <UInput v-model="state.email" />
+          </UFormGroup>
+
+          <UFormGroup label="รหัสผ่าน" name="password">
+            <UInput v-model="state.password" type="password" />
+          </UFormGroup>
+
+          <UButton type="submit" block>เข้าสู่ระบบ</UButton>
+        </UForm>
+      </UCard>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -8,4 +33,26 @@ definePageMeta({
 });
 
 async function login() {}
+
+import { object, string, type InferType } from "yup";
+import type { FormSubmitEvent } from "#ui/types";
+
+const schema = object({
+  email: string().email("Invalid email").required("Required"),
+  password: string()
+    .min(8, "Must be at least 8 characters")
+    .required("Required"),
+});
+
+type Schema = InferType<typeof schema>;
+
+const state = reactive({
+  email: undefined,
+  password: undefined,
+});
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  // Do something with event.data
+  console.log(event.data);
+}
 </script>
